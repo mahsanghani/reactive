1ed000f99775048e087cd42c7ac3a739fe50788f
mahsanghani <ghaniahsan@outlook.com>
2020-03-31 19:57:21 -0400

    variables

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from "react"
-import TodoItem from "./components/TodoItem"
+import ContactCard from "./components/ContactCard.js"
 
 function App() {
     return (
-        <div className="todo-list">
-            <TodoItem />
-            <TodoItem />
-            <TodoItem />
-            <TodoItem />
+        <div className="contacts">
+            <ContactCard
+                contact={{name: "Mr. Whiskerson", imgUrl: "http://placekitten.com/300/200", phone: "(212) 555-1234", email: "mr.whiskaz@catnap.meow"}}
+            />
+
+            <ContactCard
+                contact={{name: "Fluffykins", imgUrl: "http://placekitten.com/400/200", phone: "(212) 555-2345", email: "fluff@me.com"}}
+            />
+
+            <ContactCard
+                contact={{name: "Destroyer", imgUrl: "http://placekitten.com/400/300", phone: "(212) 555-3456", email: "ofworlds@yahoo.com"}}
+            />
+
+            <ContactCard
+                contact={{name: "Felix", imgUrl: "http://placekitten.com/200/100", phone: "(212) 555-4567", email: "thecat@hotmail.com"}}
+            />
+
         </div>
     )
 }

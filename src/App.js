12ebe086b7e1e36357a5fc75f74a25779a37d4dc
mahsanghani <ghaniahsan@outlook.com>
2020-03-31 19:38:56 -0400

    learning

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from "react"
-
-import Header from "./components/Header"
-import MainContent from "./components/MainContent"
-import Footer from "./components/Footer"
+import TodoItem from "./components/TodoItem"
 
 function App() {
     return (
-        <div>
-            <Header />
-            <MainContent />
-            <Footer />
+        <div className="todo-list">
+            <TodoItem />
+            <TodoItem />
+            <TodoItem />
+            <TodoItem />
         </div>
     )
 }
---
+++ src/components/TodoItem.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function TodoItem() {
+    return (
+        <div className="todo-item">
+            <input type="checkbox" />
+            <p>Placeholder text here</p>
+        </div>
+    )
+}
+
+export default TodoItem

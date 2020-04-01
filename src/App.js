3504bc51a154bb39c15825930cf199496139a663
mahsanghani <ghaniahsan@outlook.com>
2020-03-31 20:01:00 -0400

    kittens

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
+
 import React from "react"
-import ContactCard from "./components/ContactCard.js"
 
 function App() {
     return (
         <div className="contacts">
-            <ContactCard
-                contact={{name: "Mr. Whiskerson", imgUrl: "http://placekitten.com/300/200", phone: "(212) 555-1234", email: "mr.whiskaz@catnap.meow"}}
-            />
+            <div className="contact-card">
+                <img src="http://placekitten.com/300/200"/>
+                <h3>Mr. Whiskerson</h3>
+                <p>Phone: (212) 555-1234</p>
+                <p>Email: mr.whiskaz@catnap.meow</p>
+            </div>
 
-            <ContactCard
-                contact={{name: "Fluffykins", imgUrl: "http://placekitten.com/400/200", phone: "(212) 555-2345", email: "fluff@me.com"}}
-            />
+            <div className="contact-card">
+                <img src="http://placekitten.com/400/200"/>
+                <h3>Fluffykins</h3>
+                <p>Phone: (212) 555-2345</p>
+                <p>Email: fluff@me.com</p>
+            </div>f
 
-            <ContactCard
-                contact={{name: "Destroyer", imgUrl: "http://placekitten.com/400/300", phone: "(212) 555-3456", email: "ofworlds@yahoo.com"}}
-            />
+            <div className="contact-card">
+                <img src="http://placekitten.com/400/300"/>
+                <h3>Destroyer</h3>
+                <p>Phone: (212) 555-3456</p>
+                <p>Email: ofworlds@yahoo.com</p>
+            </div>
 
-            <ContactCard
-                contact={{name: "Felix", imgUrl: "http://placekitten.com/200/100", phone: "(212) 555-4567", email: "thecat@hotmail.com"}}
-            />
-
+            <div className="contact-card">
+                <img src="http://placekitten.com/200/100"/>
+                <h3>Felix</h3>
+                <p>Phone: (212) 555-4567</p>
+                <p>Email: thecat@hotmail.com</p>
+            </div>
         </div>
     )
 }

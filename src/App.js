c975eaaa2ce6167986104ac05af6369bd373e012
mahsanghani <ghaniahsan@outlook.com>
2020-04-05 19:50:32 -0400

    API calls

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import React, {Component} from "react";
-import conditional from "./components/conditional";
+import React, {Component} from "react"
+
+// https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/fetch
+// https://swapi.co/
+// https://medium.com/javascript-scene/master-the-javascript-interview-what-is-a-promise-27fc71e77261
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            unreadMessages: ["a", "b"]
+            loading: false,
+            character: {}
         }
     }
-    // &&
-    // false && false
+
+    componentDidMount() {
+        this.setState({loading: true})
+        fetch("https://swapi.co/api/people/1")
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    loading: false,
+                    character: data
+                })
+            })
+    }
+
     render() {
+        const text = this.state.loading ? "loading..." : this.state.character.name
         return (
             <div>
-                {
-                    this.state.unreadMessages.length > 0 &&
-                    <h2>You have {this.state.unreadMessages.length} unread messages!</h2>
-                }
+                <p>{text}</p>
             </div>
         )
     }

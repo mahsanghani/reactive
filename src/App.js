7769ea64b67c3c09eebfef45cbd8c30b8d3306bd
mahsanghani <ghaniahsan@outlook.com>
2020-04-05 20:35:37 -0400

    forms

--- src/App.js
+++ src/App.js
@@ -1,36 +1,164 @@
-import React, {Component} from "react"
+/**
+ * Challenge: Wire up the partially-finished travel form so that it works!
+ * Remember to use the concept of controlled forms
+ * https://reactjs.org/docs/forms.html
+ *
+ * All information should be populating the text below the form in real-time
+ * as you're filling it out
+ *
+ * This exercise is adapted from the V School curriculum on vanilla JS forms:
+ * https://coursework.vschool.io/travel-form/
+ *
+ * All of our challenges and learning resources are open for the public
+ * to play around with and learn from at https://coursework.vschool.io
+ */
 
-// https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/fetch
-// https://swapi.co/
-// https://medium.com/javascript-scene/master-the-javascript-interview-what-is-a-promise-27fc71e77261
+import React, {Component} from "react"
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            loading: false,
-            character: {}
+            firstName: "",
+            lastName: "",
+            age: "",
+            gender: "",
+            destination: "",
+            dietaryRestrictions: {
+                isVegan: false,
+                isKosher: false,
+                isLactoseFree: false
+            }
         }
+        this.handleChange = this.handleChange.bind(this)
     }
 
-    componentDidMount() {
-        this.setState({loading: true})
-        fetch("https://swapi.co/api/people/1")
-            .then(response => response.json())
-            .then(data => {
-                this.setState({
-                    loading: false,
-                    character: data
-                })
+    handleChange(event) {
+        const {name, value, type, checked} = event.target
+        type === "checkbox" ?
+            this.setState(prevState => {
+                return {
+                    dietaryRestrictions: {
+                        ...prevState.dietaryRestrictions,
+                        [name]: checked
+                    }
+                }
+            })
+            :
+            this.setState({
+                [name]: value
             })
     }
 
     render() {
-        const text = this.state.loading ? "loading..." : this.state.character.name
         return (
-            <div>
-                <p>{text}</p>
-            </div>
+            <main>
+                <form>
+                    <input
+                        name="firstName"
+                        value={this.state.firstName}
+                        onChange={this.handleChange}
+                        placeholder="First Name"
+                    />
+                    <br />
+
+                    <input
+                        name="lastName"
+                        value={this.state.lastName}
+                        onChange={this.handleChange}
+                        placeholder="Last Name"
+                    />
+                    <br />
+
+                    <input
+                        name="age"
+                        value={this.state.age}
+                        onChange={this.handleChange}
+                        placeholder="Age"
+                    />
+                    <br />
+
+                    <label>
+                        <input
+                            type="radio"
+                            name="gender"
+                            value="male"
+                            checked={this.state.gender === "male"}
+                            onChange={this.handleChange}
+                        /> Male
+                    </label>
+
+                    <br />
+
+                    <label>
+                        <input
+                            type="radio"
+                            name="gender"
+                            value="female"
+                            checked={this.state.gender === "female"}
+                            onChange={this.handleChange}
+                        /> Female
+                    </label>
+
+                    <br />
+
+                    <select
+                        value={this.state.destination}
+                        name="destination"
+                        onChange={this.handleChange}
+                    >
+                        <option value="">-- Please Choose a destination --</option>
+                        <option value="germany">Germany</option>
+                        <option value="norway">Norway</option>
+                        <option value="north pole">North Pole</option>
+                        <option value="south pole">South Pole</option>
+                    </select>
+
+                    <br />
+
+                    <label>
+                        <input
+                            type="checkbox"
+                            name="isVegan"
+                            onChange={this.handleChange}
+                            checked={this.state.dietaryRestrictions.isVegan}
+                        /> Vegan?
+                    </label>
+                    <br />
+
+                    <label>
+                        <input
+                            type="checkbox"
+                            name="isKosher"
+                            onChange={this.handleChange}
+                            checked={this.state.dietaryRestrictions.isKosher}
+                        /> Kosher?
+                    </label>
+                    <br />
+
+                    <label>
+                        <input
+                            type="checkbox"
+                            name="isLactoseFree"
+                            onChange={this.handleChange}
+                            checked={this.state.dietaryRestrictions.isLactoseFree}
+                        /> Lactose Free?
+                    </label>
+                    <br />
+
+                    <button>Submit</button>
+                </form>
+                <hr />
+                <h2>Entered information:</h2>
+                <p>Your name: {this.state.firstName} {this.state.lastName}</p>
+                <p>Your age: {this.state.age}</p>
+                <p>Your gender: {this.state.gender}</p>
+                <p>Your destination: {this.state.destination}</p>
+                <p>Your dietary restrictions:</p>
+                <p>Vegan: {this.state.dietaryRestrictions.isVegan ? "Yes" : "No"}</p>
+                <p>Kosher: {this.state.dietaryRestrictions.isKosher ? "Yes" : "No"}</p>
+                <p>Lactose Free: {this.state.dietaryRestrictions.isLactoseFree ? "Yes" : "No"}</p>
+            </main>
         )
     }
 }
---
+++ src/components/resources.md
@@ -0,0 +1,18 @@
+React Docs about Forms:
+https://reactjs.org/docs/forms.html
+
+https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/fetch
+https://swapi.co/
+https://medium.com/javascript-scene/master-the-javascript-interview-what-is-a-promise-27fc71e77261
+
+https://engineering.musefind.com/react-lifecycle-methods-how-and-when-to-use-them-2111a1b692b1
+https://reactjs.org/blog/2018/03/29/react-v-16-3.html#component-lifecycle-changes
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/some
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/findindex

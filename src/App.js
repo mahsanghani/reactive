5c9fce24f57c9c71d8d6b674300d2adad636d9bf
mahsanghani <ghaniahsan@outlook.com>
2020-04-06 23:16:15 -0400

    hooks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
-import React from "react"
-import Header from "./components/Header"
-import MemeGenerator from "./components/MemeGenerator"
+import React, {Component} from "react"
 
-function App() {
-    return (
-        <div>
-            <Header />
-            <MemeGenerator />
-        </div>
-    )
+class App extends Component {
+    // Change to use class properties
+    state = { firstName: "" }
+
+    // Change to use arrow functions
+    handleChange = (event) => {
+        const { name, value } = event.target
+        this.setState({
+            [name]: value
+        })
+    }
+
+    render() {
+        return (
+            <main>
+                <form>
+                    <input
+                        type="text"
+                        name="firstName"
+                        value={this.state.firstName}
+                        onChange={this.handleChange}
+                        placeholder="First Name"
+                    />
+                </form>
+                <h1>{this.state.firstName}</h1>
+            </main>
+        )
+    }
 }
 
 export default App
+
+
+/**
+ * Other modern/advanced React features/topics to learn:
+ *
+ * Official React Context API - https://reactjs.org/docs/context.html
+ * Error Boundaries - https://reactjs.org/docs/error-boundaries.html
+ * render props - https://reactjs.org/docs/render-props.html
+ * Higher Order Components - https://reactjs.org/docs/higher-order-components.html
+ * React Router - https://reacttraining.com/react-router/core/guides/philosophy
+ * React Hooks - https://reactjs.org/docs/hooks-intro.html
+ * React lazy, memo, and Suspense - https://reactjs.org/blog/2018/10/23/react-v-16-6.html
+ */

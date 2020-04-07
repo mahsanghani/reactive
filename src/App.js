43913aef8c1be98dd0a0a54bb41d8f3810422cce
mahsanghani <ghaniahsan@outlook.com>
2020-04-06 23:19:40 -0400

    create state

--- src/App.js
+++ src/App.js
@@ -1,46 +1,38 @@
-import React, {Component} from "react"
+import React, { useState } from "react"
 
-class App extends Component {
-    // Change to use class properties
-    state = { firstName: "" }
+function App() {
+    const [ answer ] = useState("Yes")
+    console.log(answer)
 
-    // Change to use arrow functions
-    handleChange = (event) => {
-        const { name, value } = event.target
-        this.setState({
-            [name]: value
-        })
+    const person = {
+        name: "Joe",
+        age: 42
     }
 
-    render() {
-        return (
-            <main>
-                <form>
-                    <input
-                        type="text"
-                        name="firstName"
-                        value={this.state.firstName}
-                        onChange={this.handleChange}
-                        placeholder="First Name"
-                    />
-                </form>
-                <h1>{this.state.firstName}</h1>
-            </main>
-        )
-    }
+    const { name, age } = person
+
+    return (
+        <div>
+            <h1>Is state important to know? {answer}</h1>
+        </div>
+    )
 }
 
+// class App extends React.Component {
+//     constructor() {
+//         super()
+//         this.state = {
+//             answer: "Yes"
+//         }
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <h1>Is state important to know? {this.state.answer}</h1>
+//             </div>
+//         )
+//     }
+// }
+
 export default App
-
-
-/**
- * Other modern/advanced React features/topics to learn:
- *
- * Official React Context API - https://reactjs.org/docs/context.html
- * Error Boundaries - https://reactjs.org/docs/error-boundaries.html
- * render props - https://reactjs.org/docs/render-props.html
- * Higher Order Components - https://reactjs.org/docs/higher-order-components.html
- * React Router - https://reacttraining.com/react-router/core/guides/philosophy
- * React Hooks - https://reactjs.org/docs/hooks-intro.html
- * React lazy, memo, and Suspense - https://reactjs.org/blog/2018/10/23/react-v-16-6.html
- */
